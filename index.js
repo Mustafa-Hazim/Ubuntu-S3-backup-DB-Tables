ea70c103a299ba0f9a014749d81d8a6a25528db6
Mustafa <115926879+Mustafa-Hazim@users.noreply.github.com>
2022-10-27 14:03:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/* 
-setting up the env vars to ensure the app works fine
-SPACES_KEY
-SPACES_SECRET
-LOCAL_DB_HOST
-LOCAL_DB_USER
-LOCAL_DB_PASSWORD
-*/
 import backup from './backup_table.js'
 
 // backup views table every day
